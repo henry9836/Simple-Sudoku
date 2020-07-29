acf90d9bf2cad6ff8a6bcdfae3981904129ff150
Henry Oliver <37818378+henry9836@users.noreply.github.com>
2020-07-29 15:19:47 +1200

    Initial Push

---
+++ js/wow.js
@@ -0,0 +1,11 @@
+function goToAdmin(){
+    window.location = "admin.html"
+}
+
+function goHome(){
+    window.location = "index.html"
+}
+
+function splashZone(){
+    alert("⚠️⚠️⚠️ You are about to enter the splash zone!!! ⚠️⚠️⚠️ \n 🎣💦");
+}
