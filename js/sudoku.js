f32f9cc648747b03ed1dd24e5a6cb50a31ee6d8f
Henry Oliver <henry983615@gmail.com>
2020-08-15 18:17:14 +1200

    redirect

--- js/sudoku.js
+++ js/sudoku.js
@@ -1,16 +1,28 @@
-console.log("Invoking jQuery...");
-console.log($("h1").text());
-console.log("jQuery Done.");
+$(document).ready(function () {
+    console.log("Invoking jQuery...");
+    console.log($("h1").text());
+    console.log($("small").text());
+    console.log("jQuery Done.");
 
-$("h1").on("click", function () {
-    $(this).text("My Final Message Goodbye");
-    $("h1").animate({ left: '250px' });
-});
+    $("h1").on("click", function () {
+        $(this).text("My Final Message Goodbye");
+        $("h1").animate({ left: '250px' });
+    });
 
-$("h1").on("mouseover", function () {
-    $(this).css("font-weight", "bold");
-});
+    $("h1").on("mouseover", function () {
+        $(this).css("font-weight", "bold");
+    });
 
-$("h1").on("mouseleave", function () {
-    $(this).css("font-weight", "normal");
+    $("h1").on("mouseleave", function () {
+        $(this).css("font-weight", "normal");
+    });
+
+    $("small").on("click", function () {
+        var validTab = window.open("https://github.com/henry9836");
+        if (validTab) {
+            win.focus();
+        } else {
+            alert('Please allow popups for this website');
+        }
+    });
 });
