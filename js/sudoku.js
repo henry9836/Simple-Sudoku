5872e906b6084cc020e154aed40e01c7739a4593
Henry <henry983615@gmail.com>
2020-09-04 10:53:23 +1200

    things are changing

--- js/sudoku.js
+++ js/sudoku.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
     console.log($("small").text());
     console.log("jQuery Done.");
 
+    var grid = new Array(new Array(9), new Array(9), new Array(9), new Array(9), new Array(9), new Array(9), new Array(9), new Array(9), new Array(9));
+
     $("h1").on("click", function () {
         $(this).text("My Final Message Goodbye");
         $("h1").animate({ left: '250px' });
@@ -35,4 +37,25 @@ $(document).ready(function () {
             alert('Please allow popups for this website');
         }
     });
+
+    $("#reset").on("click", function () {
+        console.log("reset pressed");
+
+        //init
+        for (var i = 0; i < grid.length; i++) {
+            for (var j = 0; j < grid[0].length; j++) {
+                grid[i][j] = String.valueOf(Math.floor(Math.random() * 8) + 1);
+            }
+        }
+
+        for (var i = 0; i < grid.length; i++) {
+            for (var j = 0; j < grid[0].length; j++) {
+                var element = "#" + i + j;
+                console.log(element);
+                $(element).val("4");
+            }
+        }
+
+    });
+
 });
