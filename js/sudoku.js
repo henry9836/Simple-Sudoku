ee89cc6c3ac0a1a40d3bf4d0c1767372523bc9fe
Henry <henry983615@gmail.com>
2020-09-05 10:59:33 +1200

    fixed a gen bug where cells were not checked correctly

--- js/sudoku.js
+++ js/sudoku.js
@@ -1,4 +1,6 @@
 let grid = new Array(new Array(9), new Array(9), new Array(9), new Array(9), new Array(9), new Array(9), new Array(9), new Array(9), new Array(9));
+let maxSolveSteps = 100000;
+var currentStep = 0;
 
 //Checks the grid to see if it had been solved
 function checkGridCondition() {
@@ -55,12 +57,96 @@ function checkGridCondition() {
 
     //If everything is okay then we have solved the puzzle
     return true;
+}
 
+function solveRecursive() {
+    currentStep++;
+
+    console.log("[" + currentStep.toString() + "/" + maxSolveSteps.toString() + "]Solving...");
+
+    //Debug Grid
+    //for (var y = 0; y < grid.length; y++) {
+    //    console.info(grid[y]);
+    //}
+
+    console.log("------------------------------------");
+
+    //Find an unused spot
+    var xSpot = 0;
+    var ySpot = 0;
+    var foundSpot = false
+
+    for (var x = 0; x < grid[0].length; x++) {
+        for (var y = 0; y < grid.length; y++) {
+            if (grid[y][x] == 0) {
+                xSpot = x;
+                ySpot = y;
+                foundSpot = true;
+                break;
+            }
+        }
+        if (foundSpot) {
+            break;
+        }
+    }
+
+    //we found a solution because grid is full
+    if (!foundSpot) {
+        console.log("Grid is full!")
+        return true;
+    }
+
+    //We have exceeded our limit we have failed
+    if (currentStep > maxSolveSteps) {
+        console.log("run out of steps");
+        return false;
+    }
+
+    //Try and put numbers into the grid
+    for (var i = 0; i < 9; i++) {
+        if (checkValidPosition(xSpot, ySpot, i)) {
+            //Apply number
+            grid[ySpot][xSpot] = i;
+
+            console.log("found a valid number!");
+            for (var y = 0; y < grid.length; y++) {
+                console.info(grid[y]);
+            }
+            console.log("--------------------------")
+
+            //Continue Searching Downwards
+            if (solveRecursive()) {
+                //Escape upwards
+                console.log("found a solution");
+                return true;
+            }
+
+            //This branch failed so reset number
+            grid[ySpot][xSpot] = 0;
+        }
+    }
+    
+    //Bad Number
+    return false;
 }
 
 //Solves the sudoku puzzle
 function solve() {
-    
+    //Reset step counter
+    currentStep = 0;
+
+    //While the grid is not solved and we have not exceeded our step count
+    while (!checkGridCondition() && currentStep < maxSolveSteps) {
+        solveRecursive();
+    }
+
+    if (currentStep >= maxSolveSteps) {
+        console.log("Failed To Solve!");
+    }
+    else {
+        console.log("Solved!");
+    }
+
 }
 
 //Checks if a number can be placed in a position
@@ -82,35 +168,17 @@ function checkValidPosition(x, y, val) {
         }
 
         //Find starting point to search cell
-        var searchX = 0;
-        var searchY = 0;
+        var topRow = y - (y % 3);
+        var topCol = x - (x % 3);
 
-        //X
-        if (3 < x <= 6) {
-            searchX = 3;
-        }
-        else {
-            searchX = 6;
-        }
-
-        //Y
-        if (3 < y <= 6) {
-            searcyY = 3;
-        }
-        else {
-            searchY = 6;
-        }
-
-
-        //Check cell
-        for (var tmpX = 0; tmpX < 3; tmpX++) {
-            for (var tmpY = 0; tmpY < 3; tmpY++) {
-                if (grid[searchY + tmpY][searchX + tmpX] == val) {
+        //Search the cell for the same value
+        for (var yPos = 0; yPos < 3; yPos++) {
+            for (var xPos = 0; xPos < 3; xPos++) {
+                if (grid[topRow + yPos][topCol + xPos] == val) {
                     return false;
                 }
             }
         }
-
 
         //It is valid
         return true;
@@ -215,7 +283,7 @@ $(document).ready(function () {
     });
 
     //Random Slider Value
-    $("#diffSlider").val(Math.floor(Math.random() * 26) + 15);
+    $("#diffSlider").val(Math.floor(Math.random() * 27) + 18);
 
     reset();
 
